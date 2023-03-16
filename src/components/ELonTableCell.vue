7fa7162334f11da0d89b3819a170fae7f972c375
Elon-Mao <125723026+Elon-Mao@users.noreply.github.com>
2023-03-16 19:59:23 +0800

    Update ELonTableCell.vue

--- src/components/ELonTableCell.vue
+++ src/components/ELonTableCell.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="elon-cell" :style="cellStyle" @mousedown.left="leftMouseDown">
-    <slot>{{props.data[props.column.prop]}}</slot>
+  <div class="elon-cell" :style="cellStyle" @mousedown.left="leftMouseDown" @mouseenter="mouseEnter">
+    <slot>
+      <input v-if="editing" :value="cellValue" @change="valueChange($event)" />
+      <div v-else dbclick.left="leftDBClick">{{cellValue}}</div>
+{{props.data[props.column.prop]}}
+    </slot>
   </div>
 </template>
 
 <script setup lang="ts">
   import {
+    defineExpose
     inject,
-    reactive
+    reactive,
+    computed,
+    ref
   } from 'vue'
   
   const props = defineProps({
@@ -17,16 +24,42 @@
     column: Object
   })
   const tableProps = inject('tableProps')
-  const selectedBorderStyle = inject('selectedBorderStyle')
+  const setSelectedCell = inject('setSelectedCell')
+  const setLastHoverCell = inject('setLastHoverCell')
+  const clearSelected = inject('clearSelected')
+  const updateData = inject('updateData')
   const cellStyle = reactive({
     'border-right': tableProps.border
   })
+
+  const cellValue = computed(() => props.data[props.column.prop])
+
+  const editing = ref(false)
   
   function leftMouseDown(event) {
-    const cellDom = event.target
-    console.log(this)
-    debugger
+    clearSelected()
+    setSelectedCell(props.rowIndex, props.columnIndex)
   }
+
+  function mouseEnter(event: MouseEvent) {
+    if (event.buttons !== 1) {
+      return
+    }
+    setLastHoverCell(props.rowIndex, props.columnIndex)
+  }
+
+  function leftDBClick() {
+    editing.value = true
+  }
+
+  function valueChange(event) {
+    const target = event.target as HTMLInputElement
+    updateData((draft: any) => draft[props.rowIndex][props.column.prop] = target.value)
+  }
+
+  defineExpose({
+    props
+  })
 </script>
 
 <style scoped>
